a9b55375ae33c7ceca99c538b462fb1384c79df8
Ravi Yadav <ravi.y0807@gmail.com>
2019-05-07 17:39:28 +0100

    Calculate sum of even Fibonacci Numbers.
    The commit contains the the complete code required as part of problem 2(a) i.e. to calculate the sum of even Fibonacci number within limit of n.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,21 @@ class Fibonacci{
     cpp_int m_nextEven;
 
     cpp_int even_sum(int limit) {
-        
+        if(limit == 0){
+            return 0;
+        }
+        cpp_int ef1 = 0, ef2 = 2;
+        cpp_int sum = m_previousEven + m_currentEven;
+        int count = 0;
+
+        while (count <= limit-2) {
+            count++;
+            m_nextEven = 4*m_currentEven + m_previousEven;
+            m_previousEven = m_currentEven;
+            m_currentEven = m_nextEven;
+            sum += m_currentEven;
+        }
+
         return sum;
     }
 
@@ -26,7 +40,8 @@ public:
     }
 
     cpp_int get_even_sum(int limit){
-        
+        int m_totalEvenNumber;
+        m_totalEvenNumber = limit / 3;
         return even_sum(m_totalEvenNumber);
     }
 
@@ -37,6 +52,9 @@ int main()
 {
     Fibonacci obj;
     int m_input;
-    
+    std::cout<<"Enter nth number as Limit : ";
+    std::cin>> m_input;
+    std::cout<<"Sum of Even Number within limit: ";
+    std::cout<<obj.get_even_sum(m_input);
     return 0;
 }
